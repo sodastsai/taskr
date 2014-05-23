b68d4890af55ef49b092e4f8cb90a4d852938b36
sodas tsai <sodas2002@gmail.com>
2014-05-23 22:05:31 +0800

    Update example

--- example.py
+++ example.py
@@ -3,8 +3,8 @@ from taskr import task
 @task
 @task.set_argument('source', help='The source where you come from', choices=('Tokyo', 'Osaka'))
 @task.set_argument('--speed', '-s', help='The speed you wanna run', type=int)
-def run(source, destination, speed=42):
-    print('Run from {0} to {1} by speed={2}'.format(source, destination, speed))
+def run(source, destination, speed=42, step_size=1):
+    print('Run from {0} to {1} by speed={2} and step={3}'.format(source, destination, speed, step_size))
    
 if __name__ == '__main__':
     task.dispatch()
