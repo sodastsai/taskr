441fb64f0548081898164ff57d77f6dbc5b61962
sodastsai <sodas2002@gmail.com>
2014-10-06 18:36:49 -0700

    Refine django support

--- taskr/contrib/django.py
+++ taskr/contrib/django.py
@@ -6,8 +6,7 @@ def setup_django(settings=None, project_path=None):
     # Update Python path
     sys.path = [project_path or os.getcwd()] + sys.path
     # Setup django module
-    if 'DJANGO_SETTINGS_MODULE' not in os.environ and settings:
-        os.environ['DJANGO_SETTINGS_MODULE'] = settings
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
     # Setup django
     # noinspection PyPackageRequirements
     import django
