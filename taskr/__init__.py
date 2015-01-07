45edd8de0367c26185919bbf904f7a72db858f50
sodastsai <sodas2002@gmail.com>
2015-01-07 17:06:02 +0800

    Bumpt to 0.1.39

--- taskr/__init__.py
+++ taskr/__init__.py
@@ -24,5 +24,5 @@ console = Console(sys.stdout)
 
 __all__ = [Task.__name__, Console.__name__, Color.__name__, 'task', 'console']
 
-VERSION = (0, 1, 38)
+VERSION = (0, 1, 39)
 __version__ = '.'.join(map(lambda x: str(x), VERSION))
