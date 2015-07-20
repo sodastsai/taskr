b3161ea307954b8dbbfafa34b2debfb9ebe0407f
sodastsai <sodas2002@gmail.com>
2015-07-20 12:30:52 +0800

    Bump to 0.2.28

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from __future__ import unicode_literals, division, absolute_import, print_functi
 from setuptools import setup, find_packages
 
 setup(name='taskr',
-      version='0.2.27',
+      version='0.2.28',
       description='Python Command Line Utility',
       author='sodastsai',
       author_email='sodas2002@gmail.com',
